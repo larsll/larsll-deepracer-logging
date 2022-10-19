39f408bdb5362a40c36d727a938dfb98c074c268
Lars Ludvigsen <larsll@outlook.com>
2022-10-19 06:56:18 +0000

    Initial population of files

--- logging_pkg/setup.py
+++ logging_pkg/setup.py
@@ -4,7 +4,7 @@ package_name = 'logging_pkg'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -13,13 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='ludvigse',
+    maintainer='Lars Lorentz Ludvigsen',
     maintainer_email='larsll@outlook.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='ROS2 package that enables creation of rosbags for DeepRacer',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'bag_log_node = logging_pkg.bag_log_node:main'
         ],
     },
 )
